a1841b41780e88548727b2059329b2cd566bf66d
seungahpyun <seungahpyun@icloud.com>
2025-06-23 22:28:15 +0200

    cpp02 ex02 wip

--- cpp-module-02/ex03/include/Point.hpp
+++ cpp-module-02/ex03/include/Point.hpp
@@ -6,7 +6,32 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/23 15:59:17 by spyun         #+#    #+#                 */
-/*   Updated: 2025/06/23 15:59:18 by spyun         ########   odam.nl         */
+/*   Updated: 2025/06/23 22:27:39 by seungah       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef POINT_HPP
+# define POINT_HPP
+
+#include "Fixed.hpp"
+
+class Point
+{
+    private:
+        Fixed const _x;
+        Fixed const _y;
+
+    public:
+        Point();
+        Point(const float x, const float y);
+        Point(const Point& other);
+        Point& operator=(const Point& other);
+        ~Point();
+
+        Fixed getX() const;
+        Fixed getY() const;
+};
+
+std::ostream& operator<<(std::ostream& os, const Point& point);
+
+#endif
