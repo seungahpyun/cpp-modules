582777c8dd7ccd34a3961661a0dae5b08ad6ceed
seungahpyun <spyun@student.codam.nl>
2025-08-05 09:22:17 +0200

    cpp07 ex00

--- cpp-module-07/ex00/include/whatever.hpp
+++ cpp-module-07/ex00/include/whatever.hpp
@@ -6,7 +6,39 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/08/05 09:20:07 by spyun         #+#    #+#                 */
-/*   Updated: 2025/08/05 09:20:08 by spyun         ########   odam.nl         */
+/*   Updated: 2025/08/05 09:21:50 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T>
+void swap(T &a, T &b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+const T &min(const T &a, const T &b)
+{
+	return (a < b) ? a : b;
+}
+
+template <typename T>
+const T &max(const T &a, const T &b)
+{
+	return (a > b) ? a : b;
+}
+
+template <typename T>
+void print(const T &value)
+{
+	std::cout << value << std::endl;
+}
+
+#endif
