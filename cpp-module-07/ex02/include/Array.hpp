df1419ca7f4778ac55d5525ae4c975a6233c6a26
seungahpyun <spyun@student.codam.nl>
2025-08-05 10:32:59 +0200

    cpp07 ex02 init

--- cpp-module-07/ex02/include/Array.hpp
+++ cpp-module-07/ex02/include/Array.hpp
@@ -6,7 +6,19 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/08/05 10:20:53 by spyun         #+#    #+#                 */
-/*   Updated: 2025/08/05 10:20:54 by spyun         ########   odam.nl         */
+/*   Updated: 2025/08/05 10:32:42 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <iostream>
+
+template <typename T>
+class Array
+{
+
+}
+
+#endif
