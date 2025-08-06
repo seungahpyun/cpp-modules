35101720c66bee30613170e8f5ce21916e70e6a7
seungahpyun <spyun@student.codam.nl>
2025-08-06 10:03:11 +0200

    constructor/deconstructor

--- cpp-module-07/ex02/include/Array.hpp
+++ cpp-module-07/ex02/include/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/08/05 10:20:53 by spyun         #+#    #+#                 */
-/*   Updated: 2025/08/05 10:47:21 by spyun         ########   odam.nl         */
+/*   Updated: 2025/08/06 10:01:57 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,31 @@
 template <typename T>
 class Array
 {
+	private:
+		T		*_data;
+		size_t	_size;
 
+	public:
+		Array<T>() : _size(0), _data(nullptr) {};
+		Array(size_t n) : _size(n)
+		{
+			if (n == 0)
+				_data = nullptr;
+			else
+				_data = new T[n]();
+		}
+		Array(const Array &other);
+		~Array<T>()
+		{
+			if (_data)
+				delete[] _data;
+		};
+
+		Array &operator=(const Array &other);
+		T &operator[](size_t index);
+		const T &operator[](size_t index) const;
+
+		size_t getSize() const;
 }
 
 #endif
