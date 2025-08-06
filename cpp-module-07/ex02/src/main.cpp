ba58c5042cc371aad0a1cab3f90c3e1ef81e7b25
seungahpyun <spyun@student.codam.nl>
2025-08-06 14:22:05 +0200

    adding space

--- cpp-module-07/ex02/src/main.cpp
+++ cpp-module-07/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/08/05 10:20:51 by spyun         #+#    #+#                 */
-/*   Updated: 2025/08/06 14:21:06 by spyun         ########   odam.nl         */
+/*   Updated: 2025/08/06 14:21:50 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,27 @@ int main()
 		std::cout << "=== Testing basic functionality ===" << std::endl;
 		Array<int> empty;
 		std::cout << "Empty array size: " << empty.size() << std::endl;
+
 		Array<int> intArray(5);
 		std::cout << "Int array size: " << intArray.size() << std::endl;
+
 		for (size_t i = 0; i < intArray.size(); ++i) {
 			intArray[i] = i * 10;
 		}
+
 		std::cout << "Array contents: ";
 		for (size_t i = 0; i < intArray.size(); ++i) {
 			std::cout << intArray[i] << " ";
 		}
+
 		std::cout << std::endl;
 		Array<int> copied(intArray);
 		std::cout << "Copied array size: " << copied.size() << std::endl;
+
 		Array<int> assigned;
 		assigned = intArray;
 		std::cout << "Assigned array size: " << assigned.size() << std::endl;
+		
 		std::cout << "Testing out of bounds access..." << std::endl;
 		std::cout << intArray[10] << std::endl;
 	}
