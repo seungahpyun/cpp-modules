16f4d9780965f622eac03079bc8ff6bdc2061c4d
seungahpyun <seungahpyun@icloud.com>
2025-08-07 11:33:45 +0200

    cpp08 ex01 ocf

--- cpp-module-08/ex01/src/Span.cpp
+++ cpp-module-08/ex01/src/Span.cpp
@@ -6,7 +6,24 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/08/06 14:54:40 by spyun         #+#    #+#                 */
-/*   Updated: 2025/08/06 14:54:41 by spyun         ########   odam.nl         */
+/*   Updated: 2025/08/07 11:33:03 by seungah       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Span.hpp"
+
+Span::Span(unsigned int n) : _maxSize(n) {}
+
+Span::Span(const Span &other) : _numbers(other._numbers), _maxSize(other._maxSize) {}
+
+Span &Span::operator=(const Span &other)
+{
+    if (this != &other)
+    {
+        _numbers = other._numbers;
+        _maxSize = other._maxSize;
+    }
+    return *this;
+}
+
+Span::~Span() {}
