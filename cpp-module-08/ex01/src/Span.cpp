79c2085cc44292d427fa2027f42d7e956bd51dfe
seungahpyun <seungahpyun@icloud.com>
2025-08-11 11:36:51 +0200

    cpp08 ex01 func add

--- cpp-module-08/ex01/src/Span.cpp
+++ cpp-module-08/ex01/src/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/08/06 14:54:40 by spyun         #+#    #+#                 */
-/*   Updated: 2025/08/07 11:33:03 by seungah       ########   odam.nl         */
+/*   Updated: 2025/08/11 11:36:27 by seungah       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,61 @@ Span &Span::operator=(const Span &other)
 }
 
 Span::~Span() {}
+
+void Span::addNumber(int number)
+{
+    if (_numbers.size() >= _maxSize)
+        throw std::length_error("Span is full");
+    _numbers.push_back(number);
+}
+
+template <typename InputIterator>
+void Span::addNumbers(InputIterator first, InputIterator last)
+{
+    while (first != last)
+    {
+        addNumber(*first);
+        ++first;
+    }
+}
+
+int Span::shortestSpan() const
+{
+    if (_numbers.size() < 2)
+        throw std::logic_error("Not enough numbers to find a span");
+        
+    std::vector<int> sortedNumbers = _numbers;
+    std::sort(sortedNumbers.begin(), sortedNumbers.end());
+    int minSpan = std::numeric_limits<int>::max();
+    
+    for (size_t i = 1; i < sortedNumbers.size(); ++i)
+    {
+        int span = sortedNumbers[i] - sortedNumbers[i - 1];
+        if (span < minSpan)
+            minSpan = span;
+    }
+    return minSpan;
+}
+
+int Span::longestSpan() const
+{
+    if (_numbers.size() < 2)
+        throw std::logic_error("Not enough numbers to find a span");
+    auto minMax = std::minmax_element(_numbers.begin(), _numbers.end());
+    return *minMax.second - *minMax.first;
+}
+
+unsigned int Span::size() const
+{
+    return _numbers.size();
+}
+
+unsigned int Span::maxSize() const
+{
+    return _maxSize;
+}
+
+const std::vector<int> &Span::getNumbers() const
+{
+    return _numbers;
+}
