7b1ac47fbf6079c3716a8650e19eb74ad15d324d
seungahpyun <seungahpyun@icloud.com>
2025-08-12 07:45:11 +0200

    cpp08 ex02 header added

--- cpp-module-08/ex02/include/MutantStack.hpp
+++ cpp-module-08/ex02/include/MutantStack.hpp
@@ -1,13 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                         ::::::::           */
-/*   MutantStack.hpp                                     :+:    :+:           */
-/*                                                      +:+                   */
-/*   By: spyun <spyun@student.codam.nl>                +#+                    */
-/*                                                    +#+                     */
-/*   Created: 2025/08/11 11:41:13 by spyun          #+#    #+#                */
-/*   Updated: 2025/08/11 11:41:14 by spyun          ########   odam.nl        */
+/*                                                        ::::::::            */
+/*   MutantStack.hpp                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: spyun <spyun@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/08/11 11:41:13 by spyun         #+#    #+#                 */
+/*   Updated: 2025/08/12 07:43:05 by seungah       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
 
+#include <stack>
+#include <iostream>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+public:
+    typedef typename std::stack<T>::container_type::iterator iterator;
+    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+    iterator begin() { return std::stack<T>::c.begin(); }
+    iterator end() { return std::stack<T>::c.end(); }
+    const_iterator begin() const { return std::stack<T>::c.begin(); }
+    const_iterator end() const { return std::stack<T>::c.end(); }
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const MutantStack<T> &ms)
+{
+    os << "MutantStack contents: ";
+    for (typename MutantStack<T>::const_iterator it = ms.begin(); it != ms.end(); ++it)
+    {
+        os << *it << " ";
+    }
+    return os;
+}
+
+#endif
