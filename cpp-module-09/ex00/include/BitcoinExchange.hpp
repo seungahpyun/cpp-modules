f8a01da7f02cf297634cc59424f30eff8dab2321
seungahpyun <spyun@student.codam.nl>
2025-08-18 11:06:48 +0200

    delete class

--- cpp-module-09/ex00/include/BitcoinExchange.hpp
+++ cpp-module-09/ex00/include/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/08/13 08:55:28 by spyun         #+#    #+#                 */
-/*   Updated: 2025/08/18 09:23:09 by spyun         ########   odam.nl         */
+/*   Updated: 2025/08/18 11:06:20 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,11 @@
 #include <fstream>
 #include <sstream>
 
-class BitcoinExchange
-{
-	private:
-		std::map<std::string, double> _database;
-
-		bool isValidDate(const std::string& date);
-		bool isValidValue(double value);
-		std::string trim(const std::string& str);
-		bool parseLine(const std::string& line, std::string& date, double& value, char delimiter);
-	public:
-		BitcoinExchange();
-		BitcoinExchange(const BitcoinExchange& other);
-		BitcoinExchange& operator=(const BitcoinExchange& other);
-		~BitcoinExchange();
-
-		bool loadDatabase(const std::string& filename);
-		void processInputFile(const std::string& filename);
-		double getExchangeRate(const std::string& date);
-};
+bool loadDatabase(const std::string& filename, std::map<std::string, double>& database);
+void processInputFile(const std::string& filename, const std::map<std::string, double>& database);
+double getExchangeRate(const std::string& date, const std::map<std::string, double>& database);
+bool isValidDate(const std::string& date);
+bool isValidValue(double value);
+std::string trim(const std::string& str);
 
 #endif
--- cpp-module-09/ex00/src/BitcoinExchange.cpp
+++ cpp-module-09/ex00/src/BitcoinExchange.cpp
@@ -6,44 +6,8 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/08/13 08:55:37 by spyun         #+#    #+#                 */
-/*   Updated: 2025/08/18 08:57:54 by spyun         ########   odam.nl         */
+/*   Updated: 2025/08/18 11:06:31 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
-
-#include <fstream>
-
-BitcoinExchange::BitcoinExchange() {}
-
-BitcoinExchange::BitcoinExchange(const BitcoinExchange& other) : _database(other._database) {}
-
-BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other)
-{
-	if (this != &other)
-	{
-		_database = other._database;
-	}
-	return *this;
-}
-
-BitcoinExchange::~BitcoinExchange() {}
-
-bool BitcoinExchange::loadDatabase(const std::string& filename)
-{
-	std::ifstream file(filename);
-	if (!file.is_open())
-	{
-		std::cerr << "Error: Could not open file " << filename << std::endl;
-		return false;
-	}
-
-	std::string line;
-	while (std::getline(file, line))
-	{
-		// Process each line and populate the _database
-	}
-
-	file.close();
-	return true;
-}
