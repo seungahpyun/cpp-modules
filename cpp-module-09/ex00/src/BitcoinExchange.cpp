125b34e28e2f23b58f714cb72ef0c4dbfedd9b4b
seungahpyun <spyun@student.codam.nl>
2025-08-18 08:58:27 +0200

    cpp09

--- cpp-module-09/ex00/src/BitcoinExchange.cpp
+++ cpp-module-09/ex00/src/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/08/13 08:55:37 by spyun         #+#    #+#                 */
-/*   Updated: 2025/08/14 10:04:01 by seungah       ########   odam.nl         */
+/*   Updated: 2025/08/18 08:57:54 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,30 +20,30 @@ BitcoinExchange::BitcoinExchange(const BitcoinExchange& other) : _database(other
 
 BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other)
 {
-    if (this != &other)
-    {
-        _database = other._database;
-    }
-    return *this;
+	if (this != &other)
+	{
+		_database = other._database;
+	}
+	return *this;
 }
 
 BitcoinExchange::~BitcoinExchange() {}
 
 bool BitcoinExchange::loadDatabase(const std::string& filename)
 {
-    std::ifstream file(filename);
-    if (!file.is_open())
-    {
-        std::cerr << "Error: Could not open file " << filename << std::endl;
-        return false;
-    }
+	std::ifstream file(filename);
+	if (!file.is_open())
+	{
+		std::cerr << "Error: Could not open file " << filename << std::endl;
+		return false;
+	}
 
-    std::string line;
-    while (std::getline(file, line))
-    {
-        // Process each line and populate the _database
-    }
+	std::string line;
+	while (std::getline(file, line))
+	{
+		// Process each line and populate the _database
+	}
 
-    file.close();
-    return true;
+	file.close();
+	return true;
 }
--- cpp-module-09/ex00/src/main.cpp
+++ cpp-module-09/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/08/13 08:55:50 by spyun         #+#    #+#                 */
-/*   Updated: 2025/08/13 08:58:13 by seungah       ########   odam.nl         */
+/*   Updated: 2025/08/18 08:58:02 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 
 int main(int argc, char** argv)
 {
-    if (argc != 2)
-    {
-        std::cerr << "Error: could not open file." << std::endl;
-        return 1;
-    }
+	if (argc != 2)
+	{
+		std::cerr << "Error: could not open file." << std::endl;
+		return 1;
+	}
 
-    BitcoinExchange exchange;
-    if (!exchange.loadDatabase("data.csv"))
-    {
-        std::cerr << "Error: could not load database." << std::endl;
-        return 1;
-    }
-    exchange.processInputFile(argv[1]);
-    return 0;
+	BitcoinExchange exchange;
+	if (!exchange.loadDatabase("data.csv"))
+	{
+		std::cerr << "Error: could not load database." << std::endl;
+		return 1;
+	}
+	exchange.processInputFile(argv[1]);
+	return 0;
 }
