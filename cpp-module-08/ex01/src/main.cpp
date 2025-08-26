c7df3e5657f060bc69c6417e5fcdd62126558a06
seungahpyun <spyun@student.codam.nl>
2025-08-26 10:44:21 +0200

    test

--- cpp-module-08/ex01/src/main.cpp
+++ cpp-module-08/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/08/06 14:54:43 by spyun         #+#    #+#                 */
-/*   Updated: 2025/08/12 07:56:30 by seungah       ########   odam.nl         */
+/*   Updated: 2025/08/26 09:51:24 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 
 int main()
 {
-	{	
+	{
 		std::cout << "=== Basic Test ===" << std::endl;
 		Span sp = Span(5);
 		sp.addNumber(6);
@@ -31,7 +31,7 @@ int main()
 		std::cout << "Longest span: " << sp.longestSpan() << std::endl;
 	}
 
-	{  
+	{
 		std::cout << "\n=== Large Scale Test (15,000 numbers) ===" << std::endl;
 		try
 		{
@@ -39,7 +39,7 @@ int main()
 			std::vector<int> numbers;
 			for (int i = 0; i < 15000; ++i)
 			{
-				numbers.push_back(i * 2); 
+				numbers.push_back(i * 2);
 			}
 			bigSpan.addNumbers(numbers.begin(), numbers.end());
 			std::cout << "Size: " << bigSpan.size() << std::endl;
@@ -52,7 +52,7 @@ int main()
 		}
 	}
 
-	{	
+	{
 		std::cout << "\n=== Exception Test ===" << std::endl;
 		try
 		{
